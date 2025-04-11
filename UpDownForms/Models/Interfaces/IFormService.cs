69e9c92bcb9b1726827323369c2cd90a1a6c3adc
cleite <cleite@FUJIESPR-E85-07>
2025-04-11 10:01:33 +0200

    trying to implement interfaces to make it more flexible (and testable)

---
+++ UpDownForms/Models/Interfaces/IFormService.cs
@@ -0,0 +1,16 @@
+using UpDownForms.DTO.FormDTOs;
+using UpDownForms.DTO.ResponseDTOs;
+using UpDownForms.Pagination;
+
+namespace UpDownForms.Services
+{
+    public interface IFormService
+    {
+        Task<Pageable<FormDTO>> GetForms(PageParameters pageParameters);
+        Task<FormDTO> GetForm(int id);
+        Task<FormDTO> PostForm(CreateFormDTO createFormDTO);
+        Task<FormDTO> PutForm(int id, UpdateFormDTO updateFormDTO);
+        Task<FormDTO> DeleteForm(int id);
+        Task<Pageable<ResponseDTO>> GetResponsesByFormId(int id, PageParameters pageParameters);
+    }
+}
